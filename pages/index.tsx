7a172ee8ba8640166797ac6c4659b3af98ab4b8a
Chris Sanders <astericky@gmail.com>
2021-02-11 02:16:39 -0500

    add styled components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import styled from 'styled-components'
+
+const Header = styled.header`
+  color: blue;
+  font-family: Arial, Helvetica, sans-serif;
+  &:after {
+    content: "💅"
+  }
+`
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
+    <Header>Hello styled components!</Header>
     <h1>Hello Next.js 👋</h1>
     <p>
       <Link href="/about">
